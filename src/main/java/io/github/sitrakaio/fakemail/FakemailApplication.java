3b0e479d34aa3a4be74f39af43532d11a329c037
Sitraka Ratsimba <sratsimba8@gmail.com>
2023-09-19 14:21:41 +0300

    Initial commit

---
+++ src/main/java/io/github/sitrakaio/fakemail/FakemailApplication.java
@@ -0,0 +1,13 @@
+package io.github.sitrakaio.fakemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakemailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakemailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/sitrakaio/fakemail/FakemailApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sitrakaio.fakemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakemailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
